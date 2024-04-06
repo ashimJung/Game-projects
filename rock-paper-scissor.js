a8becbad08a452b3ecf4d3e2928eac321fe67c83
ashimJung <144008982+ashimJung@users.noreply.github.com>
2024-04-06 21:26:09 +0545

    Add files via upload

---
+++ rock-paper-scissor.js
@@ -0,0 +1,117 @@
+const head=document.querySelector('.heading')
+    ;
+    head.textContent=head.textContent.toUpperCase();
+
+      const score =   JSON.parse(localStorage.getItem('score')) ||
+            {Wins:0, Loss:0,Ties:0
+        };
+        
+        updateScoreElement();
+
+
+        
+       
+    
+        function playerGame(playerMove){
+   
+     const computerMove = pickRandomMove();
+     let result = '';
+     if(playerMove==='Scissor')
+     {
+           if(computerMove==='rock'){
+             result=('You Lose.')
+           }
+           else if(computerMove=== 'paper'){
+             result=('You Win.');
+           }
+           else if(computerMove==='scissor'){
+             result=('Tie.')
+           }
+ 
+          }
+ 
+ else if(playerMove==='Rock')
+ {
+     
+     if(computerMove==='rock'){
+       result=('Tie.')
+     }
+     else if(computerMove=== 'paper'){
+       result=('You Lose.');
+     }
+     else if(computerMove==='scissor'){
+       result=('You Win.')
+     }
+     
+  
+ }
+   
+
+
+ else if (playerMove==='Paper') 
+ {
+     
+     if(computerMove==='rock'){
+       result=('You Win.')
+     }
+     else if(computerMove=== 'paper'){
+       result=('Tie.');
+     }
+     else if(computerMove==='scissor'){
+       result=('You Lose.')
+     }
+   }
+
+
+     if(result==='You Win.'){
+   score.Wins +=1;
+   }
+   else if(result==='You Lose.'){
+     score.Loss+=1;
+   }
+   else if(result==='Tie.'){
+     score.Ties +=1;
+   }
+   localStorage.setItem('score',JSON.stringify(score));
+
+   updateScoreElement();
+
+
+   document.querySelector('.js-result')
+      .innerHTML=result;
+
+   document.querySelector('.js-moves')
+      .innerHTML=` You picked  <img src="rpc-img/${playerMove}-emoji.png" alt="" class="move-icon">   - computer picked <img src="rpc-img/${computerMove}-emoji.png" alt="" class="move-icon">`;
+    
+ 
+
+     
+ 
+ 
+}
+
+ 
+ 
+ function pickRandomMove(){
+   const randomNumber = Math.random();
+   let computerMove = '';
+     if(randomNumber>=0 && randomNumber<1/3){
+       computerMove= 'rock';
+     }
+     else if(randomNumber>=1/3 && randomNumber<2/3){
+       computerMove = 'paper';
+     }
+     else if (randomNumber>=2/3 && randomNumber<1){
+       computerMove = 'scissor';
+     }
+      
+
+     return computerMove;
+ }
+  
+
+ function updateScoreElement(){
+  document.querySelector('.js-score')
+        .innerHTML=`Wins: ${score.Wins} , Losses: ${score.Loss} , Ties: ${score.Ties} `;
+ }
+    
